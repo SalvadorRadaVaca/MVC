86b01f1a7a53028314d43896caaf879f39d8a52b
unknown <j.j.virus@hotmail.com>
2020-12-03 13:12:48 -0600

    first commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('URL', 'http://localhost/MVC/');
+
+define('HOST', 'localhost');
+define('DB', 'mvc');
+define('USER', 'root');
+define('PASSWORD', 'password');
+define('CHARSET', 'utf8mb4');
+
+?>
